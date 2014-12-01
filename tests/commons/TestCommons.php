ce594e091510641301af0bb7b54209a5cb2620bf
max-borisov <max.borisov@yahoo.com>
2014-12-01 14:46:14 +0200

    up TestCommons

--- tests/commons/TestCommons.php
+++ tests/commons/TestCommons.php
@@ -1,15 +1,32 @@
 <?php
+namespace tests\commons;
+use Yii;
+
 class TestCommons
 {
-    public static function logMeIn()
+    public static function logMeIn($I)
     {
+        $I->wantTo('ensure user is guest');
+        $I->assertTrue(Yii::$app->user->isGuest);
+
         $form = new \app\models\SignInForm();
         $form->email = 'new_max@bk.ru';
-        return $form->login();
+        $I->assertTrue($form->login());
+
+        $I->wantTo('ensure user is not guest');
+        $I->assertFalse(Yii::$app->user->isGuest);
     }
 
-    public static function logMeOut()
+    public static function logMeOut($I)
     {
-        return Yii::$app->user->logout();
+        $I->wantTo('ensure user is guest again');
+        $I->assertTrue(Yii::$app->user->logout());
+        $I->assertTrue(Yii::$app->user->isGuest);
+    }
+
+    public static function categoryForm($actor, $categoryTitle)
+    {
+        $actor->fillField('input[type="text"]', $categoryTitle);
+        $actor->click('input[type="submit"]');
     }
 }
