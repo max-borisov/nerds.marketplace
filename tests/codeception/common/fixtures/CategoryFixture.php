df7d006a85343fd2abca551d2fe973be26d18e5b
max-borisov <max.borisov@yahoo.com>
2014-12-03 15:12:59 +0200

    Add fixtures for Category model

---
+++ tests/codeception/common/fixtures/CategoryFixture.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tests\codeception\common\fixtures;
+
+use yii\test\ActiveFixture;
+
+/**
+ * Category fixture
+ */
+class CategoryFixture extends ActiveFixture
+{
+    public $modelClass = '\app\models\Category';
+}
---
+++ tests/codeception/unit/fixtures/data/models/category.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'accessories' => [
+        'id'            => 1,
+        'title'         => 'Accessories',
+        'created_at'    => '1402312317',
+        'updated_at'    => '1402312317',
+    ],
+
+    'amplifiers' => [
+        'id'            => 2,
+        'title'         => 'Amplifiers',
+        'created_at'    => '1402312317',
+        'updated_at'    => '1402312317',
+    ],
+
+    'tools' => [
+        'id'            => 3,
+        'title'         => 'Tools',
+        'created_at'    => '1402312317',
+        'updated_at'    => '1402312317',
+    ],
+];
