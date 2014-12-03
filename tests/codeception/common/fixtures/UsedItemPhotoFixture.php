c8b1ff3357e5619f6936c0d1d631176df0791307
max-borisov <max.borisov@yahoo.com>
2014-12-03 17:19:03 +0200

    Add UsedItemPhoto fixture

---
+++ tests/codeception/common/fixtures/UsedItemPhotoFixture.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tests\codeception\common\fixtures;
+
+use yii\test\ActiveFixture;
+
+/**
+ * UsedItemPhoto fixture
+ */
+class UsedItemPhotoFixture extends ActiveFixture
+{
+    public $modelClass = '\app\models\UsedItemPhoto';
+}
