5a69762cf1724a1f1d77dbb5cccf5f8029f62800
max-borisov <max.borisov@yahoo.com>
2014-12-04 13:55:40 +0200

    up

--- tests/codeception/unit/models/nerds/UsedItemTest.php
+++ tests/codeception/unit/models/nerds/UsedItemTest.php
@@ -11,35 +11,6 @@ class UsedItemTest extends DbTestCase
 {
     use Specify;
 
-    /*public function testValidation()
-    {
-        $category = new Category();
-
-        $this->specify('category title is required', function () use ($category) {
-            $category->title = '';
-            expect('model should return validation error', $category->validate())->false();
-        });
-
-        $this->specify('category title is ok', function () use ($category) {
-            $category->title = 'New category title';
-            expect('model validation is successful', $category->validate())->true();
-        });
-    }
-
-    public function testPrepareDropDown()
-    {
-        $this->specify('organize categories into array', function () {
-            expect('categories list is not empty', (new Category)->prepareDropDown())->notEmpty();
-        });
-    }
-
-    public function testGetAttachedItems()
-    {
-        $this->specify('test items attached to the category', function () {
-            expect('category has attached items', Category::findOne(1)->attachedItems)->notEmpty();
-        });
-    }*/
-
     public function testGetCategory()
     {
         $this->specify('test specific item belongs to category', function () {
