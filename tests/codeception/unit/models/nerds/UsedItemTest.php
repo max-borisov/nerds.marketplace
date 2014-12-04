0ff100c47d9acf9a19fecdba2846e21a5537cd26
max-borisov <max.borisov@yahoo.com>
2014-12-04 13:31:16 +0200

    up UsedItemTest
    add search test

--- tests/codeception/unit/models/nerds/UsedItemTest.php
+++ tests/codeception/unit/models/nerds/UsedItemTest.php
@@ -5,6 +5,7 @@ namespace tests\codeception\unit\models\nerds;
 use app\models\UsedItem;
 use yii\codeception\DbTestCase;
 use Codeception\Specify;
+use Yii;
 
 class UsedItemTest extends DbTestCase
 {
@@ -68,6 +69,24 @@ class UsedItemTest extends DbTestCase
     {
         $this->specify('item has a preview', function () {
             expect('item preview', UsedItem::findOne(1)->preview)->equals('http://placehold.it/250x200');
+        });
+    }
+
+    public function testSearch()
+    {
+        $this->specify('search items according to filter parameters', function () {
+            $model = new UsedItem;
+            $model->setScenario('search');
+
+            $_GET['UsedItem']['search_text'] = 'bla bla bla oops';
+            $_GET['UsedItem']['price_min'] = '';
+            $_GET['UsedItem']['price_max'] = '';
+            expect('empty search result', $model->search(Yii::$app->request->get()))->isEmpty();
+
+            $_GET['UsedItem']['search_text'] = '';
+            $_GET['UsedItem']['price_min'] = 1;
+            $_GET['UsedItem']['price_max'] = 10000;
+            expect('empty search result', $model->search(Yii::$app->request->get()))->notEmpty();
         });
     }
 }
