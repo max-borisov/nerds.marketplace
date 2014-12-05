19efe5030f36cf11f4d27006e06ad64e6821919d
max-borisov <max.borisov@yahoo.com>
2014-12-05 14:34:42 +0200

    up topMenu links

--- views/layouts/_topMenu.php
+++ views/layouts/_topMenu.php
@@ -3,22 +3,22 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 ?>
 
-<ul class="nav navbar-nav">
-    <?php if (Yii::$app->user->isGuest) { ?>
+<?php if (Yii::$app->user->isGuest) { ?>
+    <ul class="nav navbar-nav navbar-right">
         <li class=""><?= Html::a('Sign In', Url::to('/signin')) ?></li>
-    <?php } else { ?>
+    </ul>
+<?php } else { ?>
+    <ul class="nav navbar-nav">
         <li class="<?= isset($this->params['isUsedItemPage']) ? 'active' : '' ?>">
             <?= Html::a('Used items', Url::to('/')) ?>
         </li>
         <li class="<?= isset($this->params['isCategoryPage']) ? 'active' : '' ?>">
             <?= Html::a('Categories', Url::to('/category')) ?>
         </li>
-    <?php } ?>
-</ul>
+    </ul>
 
-<?php if (!Yii::$app->user->isGuest) { ?>
-<ul class="nav navbar-nav navbar-right">
-    <li class="nav-user-name">Hi, Max</li>
-    <li><?= Html::a('Logout', '/logout') ?></li>
-</ul>
+    <ul class="nav navbar-nav navbar-right">
+        <li class="nav-user-name">Hi, <?= Html::encode(Yii::$app->user->identity->username) ?></li>
+        <li><?= Html::a('Logout', '/logout') ?></li>
+    </ul>
 <?php } ?>
