332d9a016381fd6d58595ec0fd5a1b9b0674030e
max-borisov <max.borisov@yahoo.com>
2014-12-09 15:19:01 +0200

    up NerdsAssets

--- assets/NerdsAsset.php
+++ assets/NerdsAsset.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
 
 namespace app\assets;
 
@@ -15,19 +10,13 @@ class NerdsAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/application.css',
-        'css/ekko-lightbox.min.css',
     ];
     public $js = [
-        'js/ekko-lightbox.js',
         'js/script.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
-
-        // include original file
-         'yii\bootstrap\BootstrapAsset',
-
-        // include min file
-//        '\app\components\NrdBootstrapAsset',
+        '\app\assets\BootstrapAsset',
+        '\app\assets\LightBoxAsset',
     ];
 }
