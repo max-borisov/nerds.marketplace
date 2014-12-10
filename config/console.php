74c2f8fdb63d82c1a6ba6441b5bd47be76cc3e5f
max-borisov <max.borisov@yahoo.com>
2014-12-10 15:58:42 +0200

    up console db config

--- config/console.php
+++ config/console.php
@@ -3,7 +3,7 @@
 Yii::setAlias('@tests', __DIR__ . '/tests');
 
 $params = require(__DIR__ . '/params.php');
-$db = require(__DIR__ . '/db.php');
+$db = require(__DIR__ . '/db-local.php');
 
 return [
     'id' => 'basic-console',
