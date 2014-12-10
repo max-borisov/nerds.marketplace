0bd2a60e583475df229a90aab494584a48b65729
max-borisov <max.borisov@yahoo.com>
2014-12-10 14:38:11 +0200

    up

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ .'/env-local.php';
+require(__DIR__ .'/env-local.php');
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
