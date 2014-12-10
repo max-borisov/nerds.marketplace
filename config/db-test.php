7a6a3bf6e7e1a7dba9a1275f7769d302d6592b42
max-borisov <max.borisov@yahoo.com>
2014-12-10 18:28:34 +0200

    make yii-test
    * make console file to apply migrations to test database
    * add config file for test db

---
+++ config/db-test.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'components' => [
+        'db' => [
+            'class'     => 'yii\db\Connection',
+            'dsn'       => 'mysql:host=localhost:3306;dbname=nerds_tests',
+            'username'  => 'root',
+            'password'  => 'root',
+            'charset'   => 'utf8',
+        ],
+    ]
+];
