f22b0388753bbb4fcbdc3056c3ecdeecad9012bb
max-borisov <max.borisov@yahoo.com>
2014-12-10 17:36:14 +0200

    add UsedItemType model

---
+++ models/UsedItemType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+
+/**
+ * This is the model class for table "_used_item_type".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property integer $created_at
+ * @property integer $updated_at
+ */
+class UsedItemType extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '_used_item_type';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['title'], 'string', 'max' => 100],
+            [['title'], 'unique']
+        ];
+    }
+
+    /**
+     * Retrieve data (id and title)
+     * @return array
+     */
+    public function prepareList()
+    {
+        $data = (new \yii\db\Query())
+            ->select('id, title')
+            ->from($this->tableName())
+            ->orderBy('id ASC')
+            ->all();
+        return ArrayHelper::map($data, 'id', 'title');
+    }
+
+    /**
+     * Build relation with UsedItem model
+     * @return ActiveQuery
+     */
+    public function getItems()
+    {
+        return $this->hasMany(UsedItem::className(), ['type_id' => 'id']);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+}
