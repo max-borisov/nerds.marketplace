b509ae9cd4963c60717cdd0cc2cfb3da78e5c78b
max-borisov <max.borisov@yahoo.com>
2014-12-11 16:15:54 +0200

    Add HelperPage
    * file contains headers for all pages

---
+++ components/HelperPage.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\components;
+
+use Yii;
+use yii\base\Component;
+
+class HelperPage extends Component
+{
+    const SIGN_IN_PAGE_HEADER           = 'Sign in';
+    const FRONT_PAGE_HEADER             = 'Used items catalog';
+    const USER_ITEMS_PAGE_HEADER        = 'Active items';
+    const SIGN_UP_PAGE_HEADER           = 'Sign up';
+    const CATEGORIES_PAGE_HEADER        = 'Categories';
+    const ADD_NEW_CATEGORY_PAGE_HEADER  = 'Creating category';
+    const UPDATE_CATEGORY_PAGE_HEADER   = 'Updating category';
+    const NEW_ITEM_PAGE_HEADER          = 'Add used item';
+}
