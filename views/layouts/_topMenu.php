13e03f772e66a56a3fdfeb071d2efd6dffce33e7
max-borisov <max.borisov@yahoo.com>
2014-12-11 10:46:52 +0200

    up top menu
    * add dropdown menu

--- views/layouts/_topMenu.php
+++ views/layouts/_topMenu.php
@@ -17,8 +17,16 @@ use yii\helpers\Url;
         </li>
     </ul>
 
+    <!--    Action links-->
     <ul class="nav navbar-nav navbar-right">
         <li class="nav-user-name">Hi, <?= Html::encode(Yii::$app->user->identity->username) ?></li>
-        <li><?= Html::a('Logout', '/logout') ?></li>
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Actions <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Your items</a></li>
+                <li class="divider"></li>
+                <li><?= Html::a('Logout', '/logout') ?></li>
+            </ul>
+        </li>
     </ul>
 <?php } ?>
