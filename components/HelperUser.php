fe52a3ace974e6dd54bb9e4df41bbb2035b768e1
max-borisov <max.borisov@yahoo.com>
2014-12-11 11:51:13 +0200

    add HelperUser
    * helper methods to work with users data

---
+++ components/HelperUser.php
@@ -0,0 +1,32 @@
+<?php
+namespace app\components;
+
+use Yii;
+use yii\base\Component;
+
+class HelperUser extends Component
+{
+    public static function isGuest()
+    {
+        return Yii::$app->user->isGuest;
+    }
+
+    public static function uid()
+    {
+        return Yii::$app->user->id;
+    }
+
+    public static function uIdentity()
+    {
+        return Yii::$app->user->identity;
+    }
+
+    public static function uIdentityParam($key)
+    {
+        if (!empty(Yii::$app->user->identity)
+            && isset(Yii::$app->user->identity[$key])) {
+            return Yii::$app->user->identity[$key];
+        }
+        return null;
+    }
+}
