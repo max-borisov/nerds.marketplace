157b66ffccad169e8d3ed1e2834d86fa5183c758
max-borisov <max.borisov@yahoo.com>
2014-12-17 09:47:13 +0200

    up UsedItemType test
    * add validation test
    * add save test
    * add edit test

--- tests/codeception/unit/models/nerds/UsedItemTypeTest.php
+++ tests/codeception/unit/models/nerds/UsedItemTypeTest.php
@@ -19,6 +19,45 @@ class UsedItemTypeTest extends DbTestCase
         ];
     }
 
+    public function testValidation()
+    {
+        $this->specify('validate item with empty title', function () {
+            $type = new UsedItemType();
+            $type->title = null;
+            expect('validation error', $type->validate())->false();
+        });
+
+        $this->specify('item title should be unique', function () {
+            $type = new UsedItemType();
+            $type->title = $this->type('sale')->title;
+            expect('validation error', $type->validate())->false();
+        });
+
+        $this->specify('set correct item title', function () {
+            $type = new UsedItemType();
+            $type->title = 'brand new title';
+            expect('validation success', $type->validate())->true();
+        });
+    }
+
+    public function testSave()
+    {
+        $this->specify('create new type', function () {
+            $type = new UsedItemType();
+            $type->title = 'brand new title';
+            expect('save success', $type->save(true))->true();
+        });
+    }
+
+    public function testEdit()
+    {
+        $this->specify('edit type', function () {
+            $type = UsedItemType::findOne($this->type('edit')->id);
+            $type->title = 'edited title';
+            expect('save success', $type->save(true))->true();
+        });
+    }
+
     public function testPrepareList()
     {
         $this->specify('organize item types into array', function () {
@@ -29,7 +68,7 @@ class UsedItemTypeTest extends DbTestCase
     public function testGetItems()
     {
         $this->specify('test specific type has many items belong to it', function () {
-            expect('items list is not empty', UsedItemType::findOne(1)->items)->notEmpty();
+            expect('items list is not empty', UsedItemType::findOne($this->type('sale')->id)->items)->notEmpty();
         });
     }
 }
