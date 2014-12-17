64324a7c122277a976e2d727605435542225e280
max-borisov <max.borisov@yahoo.com>
2014-12-17 11:11:41 +0200

    Add SignInFormTest

---
+++ tests/codeception/unit/models/nerds/SignInFormTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace tests\codeception\unit\models\nerds;
+
+use Yii;
+use app\components\HelperUser;
+use app\models\SignInForm;
+use Codeception\Specify;
+use yii\codeception\TestCase;
+use app\tests\codeception\unit\fixtures\PhpbbUserFixture;
+
+
+class SignInFormTest extends TestCase
+{
+    use Specify;
+
+    public function fixtures()
+    {
+        return [
+            'user' => PhpbbUserFixture::className(),
+        ];
+    }
+
+    public function testValidation()
+    {
+        $model = new SignInForm();
+
+        $this->specify('validate model with empty attributes', function () use ($model) {
+            expect('email validation errors'            , $model->validate(['email']))->false();
+            expect('password validation errors'         , $model->validate(['password']))->false();
+        });
+
+        $this->specify('email has incorrect format', function() use ($model) {
+            $model->email = 'abk.ru';
+            expect('email validation error', $model->validate(['email']))->false();
+        });
+
+        $this->specify('set correct email and incorrect password', function() use ($model) {
+            $model->email = $this->user('max')->user_email;
+            $model->password = 'asde';
+            expect('validate password error', $model->validate())->false();
+        });
+
+        $this->specify('set correct email and password', function() use ($model) {
+            $model->email = $this->user('max')->user_email;
+            $model->password = '111111';
+            expect('validation success', $model->validate())->true();
+        });
+    }
+
+    public function testLogin()
+    {
+        $this->specify('user is Guest', function() {
+            expect('user is guest', HelperUser::isGuest())->true();
+        });
+
+        $this->specify('login user by email and password', function() {
+            $model = new SignInForm();
+            $model->email = $this->user('max')->user_email;
+            $model->password = '111111';
+            expect('login success', $model->login())->true();
+        });
+
+        $this->specify('user is not Guest', function() {
+            expect('user is not guest', HelperUser::isGuest())->false();
+        });
+    }
+}
