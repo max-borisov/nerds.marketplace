9d5ee8f68654360a74380dce46d8266554e2ae86
max-borisov <max.borisov@yahoo.com>
2014-12-18 22:05:07 +0200

    add functional tests for CategoryDeletePage

---
+++ tests/codeception/functional/nerds/CategoryDeletePageCept.php
@@ -0,0 +1,37 @@
+<?php
+
+use tests\common\TestCommon;
+use app\components\HelperPage;
+
+$I = new FunctionalTester($scenario);
+$I->wantTo('ensure that delete category page works');
+
+TestCommon::logMeIn($I);
+
+// Delete Category with no attached items
+$I->amOnPage('/category/delete/6');
+$I->expect('to be redirected to categories page');
+$I->seeCurrentUrlEquals('/category');
+$I->expectTo('see success message');
+$I->seeElement('.alert-success');
+
+// Delete Category with attached items
+$I->amOnPage('/category/delete/7');
+$I->expect('to be redirected to categories page');
+$I->seeCurrentUrlEquals('/category');
+$I->expectTo('see success message');
+$I->seeElement('.alert-success');
+
+// Go to front page
+$I->wantTo('ensure that items catalog page works after category has been deleted');
+$I->amOnPage('/');
+$I->expectTo('see correct header');
+$I->see(HelperPage::FRONT_PAGE_HEADER, 'h1');
+
+// Go to user items page
+$I->wantTo('ensure that user items page works after category has been deleted');
+$I->amOnPage('/items');
+$I->expectTo('see correct header');
+$I->see(HelperPage::USER_ITEMS_PAGE_HEADER, 'h1');
+
+TestCommon::logMeOut($I);
