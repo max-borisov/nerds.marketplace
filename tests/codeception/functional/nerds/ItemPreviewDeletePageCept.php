e62b46fdcc7e527b8ce0e3e42c0118262d504c2c
max-borisov <max.borisov@yahoo.com>
2014-12-22 14:59:58 +0200

    add functional test
    * test delete item preview url
    * update related fixtures

---
+++ tests/codeception/functional/nerds/ItemPreviewDeletePageCept.php
@@ -0,0 +1,18 @@
+<?php
+
+use tests\common\TestCommon;
+use app\components\HelperPage;
+
+$I = new FunctionalTester($scenario);
+
+TestCommon::logMeIn($I);
+
+$I->wantTo('ensure that delete item preview link works');
+$I->amOnPage('/item/preview/delete/3');
+
+$I->seeElement('.alert.alert-success');
+$I->see('Preview has been delete');
+$I->expectTo('see Edit item page header');
+$I->see(HelperPage::EDIT_ITEM_PAGE_HEADER, 'h1');
+
+TestCommon::logMeOut($I);
