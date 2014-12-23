6b13402debd26fa4ce2cc3eef05468617c8375d0
max-borisov <max.borisov@yahoo.com>
2014-12-23 21:13:14 +0200

    up acceptance config

--- tests/codeception/config/acceptance.php
+++ tests/codeception/config/acceptance.php
@@ -2,10 +2,18 @@
 /**
  * Application configuration for acceptance tests
  */
+
+$_SERVER['SCRIPT_FILENAME'] = YII_TEST_ENTRY_FILE;
+$_SERVER['SCRIPT_NAME'] = YII_TEST_ENTRY_URL;
+
+require_once Yii::getAlias('@tests') . '/codeception/common/TestCommon.php';
+
 return yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../../../config/web.php'),
     require(__DIR__ . '/config.php'),
     [
-
+        /*'params' => [
+            'hosts' => 'http://local.marketplace.nerds/index-test.php',
+        ]*/
     ]
 );
