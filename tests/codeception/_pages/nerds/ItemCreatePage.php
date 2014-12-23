34ae653128bd403db285b21c134511ad1c511900
max-borisov <max.borisov@yahoo.com>
2014-12-23 21:13:42 +0200

    up CreateItem test page

--- tests/codeception/_pages/nerds/ItemCreatePage.php
+++ tests/codeception/_pages/nerds/ItemCreatePage.php
@@ -13,39 +13,36 @@ class ItemCreatePage extends BasePage
     public $route = '/marketplace/create';
 
     /**
-     * @param array $params List of form fields params
+     * @param array $params array $params List of form fields params
      */
-    public function sendForm($params = [])
+    public function fillForm($params = [])
     {
-        if ($params) {
-            if (isset($params['warranty'])) {
-                $this->actor->fillField('input[name="UsedItem[warranty]"]', $params['warranty']);
-            }
-            if (isset($params['invoice'])) {
-                $this->actor->fillField('input[name="UsedItem[invoice]"]', $params['invoice']);
-            }
-            if (isset($params['packaging'])) {
-                $this->actor->fillField('input[name="UsedItem[packaging]"]', $params['packaging']);
-            }
-            if (isset($params['manual'])) {
-                $this->actor->fillField('input[name="UsedItem[manual]"]', $params['manual']);
-            }
-            if (isset($params['price'])) {
-                $this->actor->fillField('input[name="UsedItem[price]"]', $params['price']);
-            }
-            if (isset($params['category_id'])) {
-                $this->actor->selectOption('form select[name="UsedItem[category_id]"]', $params['category_id']);
-            }
-            if (isset($params['title'])) {
-                $this->actor->fillField('input[name="UsedItem[title]"]', $params['title']);
-            }
-            if (isset($params['type_id'])) {
-                $this->actor->fillField('input[name="UsedItem[type_id]"]', $params['type_id']);
-            }
-            if (isset($params['description'])) {
-                $this->actor->fillField('textarea[name="UsedItem[description]"]', $params['description']);
-            }
+        if (isset($params['warranty'])) {
+            $this->actor->fillField('input[name="UsedItem[warranty]"]', $params['warranty']);
         }
-        $this->actor->click('input[type="submit"]');
+        if (isset($params['invoice'])) {
+            $this->actor->fillField('input[name="UsedItem[invoice]"]', $params['invoice']);
+        }
+        if (isset($params['packaging'])) {
+            $this->actor->fillField('input[name="UsedItem[packaging]"]', $params['packaging']);
+        }
+        if (isset($params['manual'])) {
+            $this->actor->fillField('input[name="UsedItem[manual]"]', $params['manual']);
+        }
+        if (isset($params['price'])) {
+            $this->actor->fillField('input[name="UsedItem[price]"]', $params['price']);
+        }
+        if (isset($params['category_id'])) {
+            $this->actor->selectOption('form select[name="UsedItem[category_id]"]', $params['category_id']);
+        }
+        if (isset($params['title'])) {
+            $this->actor->fillField('input[name="UsedItem[title]"]', $params['title']);
+        }
+        if (isset($params['type_id'])) {
+            $this->actor->fillField('input[name="UsedItem[type_id]"]', $params['type_id']);
+        }
+        if (isset($params['description'])) {
+            $this->actor->fillField('textarea[name="UsedItem[description]"]', $params['description']);
+        }
     }
 }
