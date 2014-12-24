f5f7a972eeacada08c8ddcf22ae1fc68bd8b70f3
max-borisov <max.borisov@yahoo.com>
2014-12-24 10:50:05 +0200

    add func tests for edit item page
    * test upload files

---
+++ tests/codeception/functional/nerds/item/ItemEditPageUploadCorrectFileCept.php
@@ -0,0 +1,23 @@
+<?php
+
+use tests\common\TestCommon;
+use app\components\HelperPage;
+
+$I = new FunctionalTester($scenario);
+
+TestCommon::logMeIn($I);
+
+$I->wantTo('ensure that upload preview works for edit item page');
+$I->amOnPage('/item/edit/1');
+
+$I->expectTo('see correct title');
+$I->see(HelperPage::EDIT_ITEM_PAGE_HEADER, 'h1');
+
+$I->amOnPage('/item/edit/1');
+$I->amGoingTo('Attache correct file');
+$I->attachFile('input[type="file"]', 'preview.jpg');
+$I->click('#form-upload-images input[type="submit"]');
+$I->expectTo('see success message');
+$I->seeElement('.alert-success');
+
+TestCommon::logMeOut($I);
---
+++ tests/codeception/functional/nerds/item/ItemEditPageUploadIncorrectFileCept.php
@@ -0,0 +1,23 @@
+<?php
+
+use tests\common\TestCommon;
+use app\components\HelperPage;
+
+$I = new FunctionalTester($scenario);
+
+TestCommon::logMeIn($I);
+
+$I->wantTo('ensure that upload preview works for edit item page');
+$I->amOnPage('/item/edit/1');
+
+$I->expectTo('see correct title');
+$I->see(HelperPage::EDIT_ITEM_PAGE_HEADER, 'h1');
+
+$I->amGoingTo('Attache incorrect file');
+$I->attachFile('input[type="file"]', 'file.txt');
+$I->click('#form-upload-images input[type="submit"]');
+$I->expectTo('see error message');
+$I->see('Please fix the following errors:');
+$I->see('Only files with these extensions are allowed: jpg, png');
+
+TestCommon::logMeOut($I);
