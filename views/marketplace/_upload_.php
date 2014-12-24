05bae08a9283fec4238f5fbae84c5f5cd5054496
max-borisov <max.borisov@yahoo.com>
2014-12-24 14:10:28 +0200

    up edit item page
    * back is default view for input file

--- views/marketplace/_upload_.php
+++ views/marketplace/_upload_.php
@@ -1,24 +1,21 @@
 <?php
 use yii\helpers\Html;
 
-echo '<hr>';
 echo Html::beginForm(
     '/item/upload',
     'post',
-    ['enctype' => 'multipart/form-data', 'id' => 'form-upload-images', 'class' => 'form-horizontal']) ?>
+    ['enctype' => 'multipart/form-data', 'id' => 'form-upload-images']) ?>
+    <span class="btn btn-success fileinput-button">
+        <i class="glyphicon glyphicon-plus"></i>
+        <span>Select files...</span>
         <?= Html::activeHiddenInput($model, 'item_id', ['value' => $item->id]); ?>
-
-        <div class="form-group">
         <?php
         if (YII_ENV === 'test') {
             echo Html::activeFileInput($model, 'file[0]', ['id' => 'fileupload', 'multiple' => '']);
             echo Html::submitInput('Upload');
         } else {
-            echo '<label class="col-sm-2 control-label">Previews:</label>';
-            echo '<div class="col-sm-8">';
-                echo Html::activeFileInput($model, 'file[]', ['id' => 'fileupload', 'multiple' => '']);
-            echo '</div>';
+            echo Html::activeFileInput($model, 'file[]', ['id' => 'fileupload', 'multiple' => '']);
         }
         ?>
-        </div>
+    </span>
 <?= Html::endForm() ?>
