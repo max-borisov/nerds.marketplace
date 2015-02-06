039348b33e1a90e4a8d86fb482cfc9bcefdb28c5
max-borisov <max.borisov@yahoo.com>
2015-02-06 10:54:53 +0200

    up bash scripts

---
+++ shell/parser-all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "[---] START. All parsers."
+
+sh parser-items-hifi.sh
+
+sh parser-news-hifi.sh
+sh parser-news-rec.sh
+
+sh parser-reviews-hifi.sh
+sh parser-reviews-rec.sh
+
+echo "[---] END. All parsers."
