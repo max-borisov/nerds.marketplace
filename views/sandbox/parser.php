8f08941682d5a4971fe778b50b479446ab9412ce
max-borisov <max.borisov@yahoo.com>
2015-03-07 15:00:03 +0200

    Add form to test catalog url parser.

---
+++ views/sandbox/parser.php
@@ -0,0 +1,7 @@
+<div>
+    <form action="">
+        <input type="text" name="url" style="width: 500px" value="<?= isset($_GET['url']) ? $_GET['url'] : '' ?>"/>
+        <input type="submit" name="send">
+    </form>
+</div>
+<hr>
